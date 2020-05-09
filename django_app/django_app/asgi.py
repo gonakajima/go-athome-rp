bc03bb11c12e51b67d4ed831e67d64ab8e9d9d6c
gonakajima <gonakajima_mail@yahoo.co.jp>
2020-05-09 13:40:17 +0900

    nginx re-install

--- django_app/django_app/asgi.py
+++ django_app/django_app/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_app project.
+ASGI(Asynchronous Server Gateway Interface) config for django_app project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
